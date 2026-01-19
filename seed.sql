9522ca5e59a7a17e39d0c5d9d6b6805c5440e213
Maya Alicia <mayaraalicia64@gmail.com>
2026-01-19 19:41:50 -0300

    Add files via upload

---
+++ seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO `clientes` (`Cliente_id`, `nome`, `telefone`, `endereco`, `data_cadastro`) VALUES
+(1, 'Fulana', '(99) 99999-9999', 'Rua DeTal, 0', '1995-05-23 00:00:00'),
+(2, 'Cicrana', '(99) 99999-9999', 'Rua Dos Bobos, 0', '1991-02-20 01:23:45'),
+(3, 'Beltrano', '(99) 99999-9999', 'Avenida Aquela lá, 171', '1973-09-01 20:56:52'),
